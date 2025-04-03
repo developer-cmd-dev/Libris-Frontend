67ff34b49150cba4f7c91773fd250e03f2a152d6
developer-cmd-dev <devkmandal0@gmail.com>
2025-04-03 21:03:23 +0530

    add routing and new pages; implement theme toggle and various UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
-
+import HomePage from "./Pages/HomePage";
 
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className="w-full h-screen bg-black">
+        <HomePage/>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SignUpPage from './Pages/SignUpPage'
+import LoginPage from './Pages/LoginPage'
+import{createBrowserRouter,RouterProvider} from "react-router-dom"
+import HomePage from './Pages/HomePage'
+import { ThemeProvider } from './components/ThemProvider'
+
+
+const router = createBrowserRouter([
+
+
+  {
+    path:'/',
+    element:<App/>
+  },
+  {
+    path:'/signup',
+    element:<SignUpPage/>
+  },
+  {
+    path:'/login',
+    element:<LoginPage/>
+  }
+
+
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      </ThemeProvider>
+    </RouterProvider>
   </StrictMode>,
 )

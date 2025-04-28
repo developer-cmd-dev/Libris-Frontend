2b86dfb075a521b37d43a9b6ab81c955bf429f76
Dev Kumar <devkmandal0@gmail.com>
2025-04-28 19:50:30 +0530

    fix: adjust spacing in HomePage component for improved layout consistency

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -29,7 +29,7 @@ function HomePage() {
 
 
   return (
-    !isLoading &&    <div className="flex flex-1 flex-col gap-4 p-4">
+    !isLoading &&    <div className="flex flex-1 flex-col gap-4 p-4 ">
     <div className="grid auto-rows-min gap-4 md:grid-cols-5">
       {resData.map((data) => (
         <Link

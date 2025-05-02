4c08a9805c358684ac420fb7904046dd3179020c
Dev Kumar <devkmandal0@gmail.com>
2025-05-02 21:09:56 +0530

    fix: correct layout structure in HomePage component for improved styling

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -29,9 +29,9 @@ function HomePage() {
 
 
   return (
-    !isLoading &&    <div className="flex flex-1 flex-col gap-4 p-4 ">
+       <div className="flex flex-1 flex-col gap-4 p-4 borders border-red-300 ">
     <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-      {resData.map((data) => (
+    {!isLoading &&  resData.map((data) => (
         <Link
         to={`title/${data.title}`}
         onClick={()=>handleSendBooksData(data)}

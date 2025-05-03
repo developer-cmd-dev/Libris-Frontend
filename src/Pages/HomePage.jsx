a0eefc3fa8ea2b6c75c15fc1eb131fb18db32aa1
Dev Kumar <devkmandal0@gmail.com>
2025-05-03 20:55:45 +0530

    refactor: improve code formatting and structure in HomePage component for better readability

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -10,44 +10,44 @@ import { CiShoppingCart } from "react-icons/ci";
 
 
 function HomePage() {
-    const[resData,setResData]=useState([])
- const [response]=useOutletContext()
- const dispatch = useDispatch()
- const {isLoading} = useSelector((state) => state.loading);
+  const [resData, setResData] = useState([])
+  const [response] = useOutletContext()
+  const dispatch = useDispatch()
+  const { isLoading } = useSelector((state) => state.loading);
 
- useEffect(() => {
+  useEffect(() => {
     setResData(response)
- }, [response])
+  }, [response])
 
 
- const handleSendBooksData = (value) => {
-     dispatch(handleBooksData(value))
-   }
+  const handleSendBooksData = (value) => {
+    dispatch(handleBooksData(value))
+  }
 
- 
- 
+
+
 
 
   return (
-       <div className="flex flex-1 flex-col gap-4 p-4 borders border-red-300 ">
-    <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-    {!isLoading &&  resData.map((data) => (
-        <Link
-        to={`title/${data.title}`}
-        onClick={()=>handleSendBooksData(data)}
-          key={data.id}
-          className="aspect-square rounded-xl bg-none  flex flex-col  items-center justify-end    cursor-pointer"
-        >
+    <div className="flex flex-1 flex-col gap-4 p-4 borders border-red-300 ">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-5">
+        {!isLoading && resData.map((data) => (
+          <Link
+            to={`title/${data.title}`}
+            onClick={() => handleSendBooksData(data)}
+            key={data.id}
+            className="aspect-square rounded-xl bg-none  flex flex-col  items-center justify-end    cursor-pointer"
+          >
             <div>
               <img src={data.formats["image/jpeg"]} alt={`${data.title.concat("...")}`} />
             </div>
-          <div className="w-full h-[20%] flex flex-col px-2 items-center justify-center ">
-            <Button variant={"default"} className={"w-[70%] rounded-tl-xl rounded-tr-none rounded-bl-none rounded-br-xl   "} >Rent Book</Button>
-          </div>
-        </Link>
-      ))}
+            <div className="w-full h-[20%] flex flex-col px-2 items-center justify-center ">
+              <Button variant={"default"} className={"w-[70%] rounded-tl-xl rounded-tr-none rounded-bl-none rounded-br-xl   "} >Rent Book</Button>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
   )
 }
 
